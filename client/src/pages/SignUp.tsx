42fe7b4e016d9885eefa971d95a1e7986c8e63ef
DONG WOONG OH <integral5137@gmail.com>
2021-12-11 16:02:42 +0900

    fix validate in sign up

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -23,6 +23,7 @@ const SignUp = () => {
     formState: { errors },
   } = useForm<SignUpFormInput>({
     mode: 'onChange',
+    defaultValues: { role: 'client' },
   })
 
   const onSubmit = () => {
@@ -39,21 +40,35 @@ const SignUp = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <input
-            {...register('accountId', { required: 'Is required' })}
+            {...register('accountId', {
+              required: 'You must specify a account id',
+            })}
             type="text"
             name="accountId"
             placeholder="Account ID"
             autoComplete="off"
           />
           <button>Check</button>
+          {errors.accountId && (
+            <FormError errorMessage={errors.accountId.message!} />
+          )}
         </div>
         <div>
           <input
-            {...register('password')}
+            {...register('password', {
+              required: 'You must specify a password',
+              pattern: {
+                value: /(?=.*\d)(?=.*[a-z]).{8,}/,
+                message: 'Password must have at least 8 characters',
+              },
+            })}
             type="password"
             name="password"
             placeholder="Password"
           />
+          {errors.password && (
+            <FormError errorMessage={errors.password.message!} />
+          )}
         </div>
         <div>
           <input
@@ -71,23 +86,31 @@ const SignUp = () => {
         </div>
         <div>
           <input
-            {...register('email', { required: 'Is required' })}
+            {...register('email', {
+              required: 'You must specify a email',
+            })}
             type="email"
             name="email"
             placeholder="Email"
             autoComplete="off"
           />
           <button>Check</button>
+          {errors.email && <FormError errorMessage={errors.email.message!} />}
         </div>
         <div>
           <input
-            {...register('nickname', { required: 'Is required' })}
+            {...register('nickname', {
+              required: 'You must specify a nickname',
+            })}
             type="text"
             name="nickname"
             placeholder="Nickname"
             autoComplete="off"
           />
           <button>Check</button>
+          {errors.nickname && (
+            <FormError errorMessage={errors.nickname.message!} />
+          )}
         </div>
         <div>
           <div>
