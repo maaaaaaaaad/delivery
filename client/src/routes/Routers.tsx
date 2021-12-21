dbe0d558859f965ee9b09cdb07712f339afa933d
DONG WOONG OH <integral5137@gmail.com>
2021-12-21 18:01:44 +0900

    fix router

--- client/src/routes/Routers.tsx
+++ client/src/routes/Routers.tsx
@@ -3,13 +3,12 @@ import { Routes, Route } from 'react-router-dom'
 import Main from '../pages/Main'
 import NotFoundPage from '../pages/404'
 import Profile from '../pages/Profile'
-import { isLoggedInVar } from '../apollo'
 
 const Routers: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      {isLoggedInVar() && <Route path="/me/:nickname" element={<Profile />} />}
+      <Route path="/me/:nickname" element={<Profile />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
