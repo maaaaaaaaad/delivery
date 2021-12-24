d76265a8359655173234f94c53ecdd7519de0ab0
DONG WOONG OH <integral5137@gmail.com>
2021-12-24 22:08:02 +0900

    add upload the avatar img form

--- client/src/components/images/avatar.tsx
+++ client/src/components/images/avatar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 interface AvatarProp {
   image: any
@@ -6,9 +6,31 @@ interface AvatarProp {
 }
 
 const Avatar: React.FC<AvatarProp> = ({ image, title }) => {
+  const fileRef = useRef<HTMLInputElement>(null)
+
+  const onClick = () => {
+    fileRef.current?.click()
+  }
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = e.currentTarget.files as FileList
+    const file = fileList[0] as File
+    console.log(file)
+  }
+
   return (
     <section>
-      <img className="w-64 h-64 rounded-lg" src={image} alt={title} />
+      <div onClick={onClick} className="p-0 m-0 rounded-full cursor-pointer">
+        <img className="w-64 h-64 hover:opacity-60" src={image} alt={title} />
+      </div>
+      <input
+        onChange={onChange}
+        ref={fileRef}
+        className="hidden"
+        type="file"
+        name="file"
+        accept="image/*"
+      />
     </section>
   )
 }
