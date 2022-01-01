6d384569c3c88064570ec872ca45454ba71b0d71
DONG WOONG OH <integral5137@gmail.com>
2022-01-01 18:31:07 +0900

    reset

---
+++ server/src/common/dtos/pagination.dto.ts
@@ -0,0 +1,14 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql'
+import { RequiredOutputDto } from './required.dto'
+
+@InputType()
+export class PaginationInputDto {
+  @Field((type) => Number, { defaultValue: 1 })
+  page: number
+}
+
+@ObjectType()
+export class PaginationOutputDto extends RequiredOutputDto {
+  @Field((type) => Number, { nullable: true })
+  total?: number
+}
