861a8466dddb5077406e94e8a431d11b4e48524e
DONG WOONG OH <integral5137@gmail.com>
2022-01-04 05:52:31 +0900

    add create dto the order

---
+++ server/src/order/dtos/create.dto.ts
@@ -0,0 +1,12 @@
+import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql'
+import { OrderEntity } from '../entites/order.entity'
+import { RequiredOutputDto } from '../../common/dtos/required.dto'
+
+@InputType()
+export class CreateOrderInputDto extends PickType(OrderEntity, ['foods']) {
+  @Field((returns) => Number)
+  storeId: number
+}
+
+@ObjectType()
+export class CreateOrderOutputDto extends RequiredOutputDto {}
