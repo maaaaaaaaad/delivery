25c7cb3764a0bc0aaf52f3fc119b6bce0b29d040
DONG WOONG OH <integral5137@gmail.com>
2022-01-04 10:04:27 +0900

    fix create order dto

--- server/src/order/dtos/create.dto.ts
+++ server/src/order/dtos/create.dto.ts
@@ -1,11 +1,24 @@
-import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql'
-import { OrderEntity } from '../entites/order.entity'
+import { Field, InputType, ObjectType } from '@nestjs/graphql'
 import { RequiredOutputDto } from '../../common/dtos/required.dto'
+import { FoodOptions } from '../../stores/entities/food.entity'
+
+@InputType({ isAbstract: true })
+@ObjectType()
+export class CreateOrderItemInputDto {
+  @Field((returns) => Number)
+  foodId: number
+
+  @Field((returns) => FoodOptions, { nullable: true })
+  options?: FoodOptions[]
+}
 
 @InputType()
-export class CreateOrderInputDto extends PickType(OrderEntity, ['orderItems']) {
+export class CreateOrderInputDto {
   @Field((returns) => Number)
   storeId: number
+
+  @Field((returns) => [CreateOrderItemInputDto])
+  orderItems: CreateOrderItemInputDto[]
 }
 
 @ObjectType()
