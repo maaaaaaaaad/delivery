d9d09a39058f0725b344a6c81f01e1146d4bf752
DONG WOONG OH <integral5137@gmail.com>
2022-01-16 15:54:38 +0900

    add create payment resolver

--- server/src/payment/payment.resolver.ts
+++ server/src/payment/payment.resolver.ts
@@ -1,8 +1,25 @@
-import { Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
 import { PaymentEntity } from './entites/payment.entity'
 import { PaymentService } from './payment.service'
+import {
+  CreatePaymentInputDto,
+  CreatePaymentOutputDto,
+} from './dtos/create.dto'
+import { UseGuards } from '@nestjs/common'
+import { OwnerGuard } from '../auth/owner.guard'
+import { AuthUser } from '../auth/auth.decorator'
+import { UsersEntity } from '../users/entities/users.entity'
 
 @Resolver((of) => PaymentEntity)
 export class PaymentResolver {
   constructor(private readonly paymentService: PaymentService) {}
+
+  @UseGuards(OwnerGuard)
+  @Mutation((returns) => CreatePaymentOutputDto)
+  async createPayment(
+    @AuthUser() owner: UsersEntity,
+    @Args('input') createPaymentInputDto: CreatePaymentInputDto,
+  ): Promise<CreatePaymentOutputDto> {
+    return await this.paymentService.createPayment(owner, createPaymentInputDto)
+  }
 }
