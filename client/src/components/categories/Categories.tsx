fc6f14d0e07ae160a92ea1aef51bac4895f3d805
DONG WOONG OH <integral5137@gmail.com>
2022-01-18 19:41:46 +0900

    update category cover image

--- client/src/components/categories/Categories.tsx
+++ client/src/components/categories/Categories.tsx
@@ -7,9 +7,11 @@ interface Prop {
 
 const Categories: React.FC<Prop> = ({ category }) => {
   return (
-    <section className="bg-green-500 mr-5 p-2 rounded relative">
-      <p className="cursor-pointer font">{category.name}</p>
-      <span className="bg-red-400 absolute px-2 rounded">
+    <section
+      className="bg-cover mr-5 p-2 rounded-full relative w-12 h-12"
+      style={{ backgroundImage: `url(${category.coverImage})` }}
+    >
+      <span className="bg-red-400 absolute px-2 rounded left-8 top-8">
         {category.storeCount}
       </span>
     </section>
