17875694b1308388ed8e3da23e9d8ab5bb5179f1
DONG WOONG OH <integral5137@gmail.com>
2022-01-22 17:29:08 +0900

    add search effect

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -2,19 +2,35 @@ import React, { useEffect } from 'react'
 import Empty from '../components/block/empty'
 import { Helmet } from 'react-helmet-async'
 import { HELMET_TITLE } from '../common/constatns'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useLazyQuery } from '@apollo/client'
+import { GET_SEARCH_STORES } from '../graphql/queries/queries'
 
 const Search = () => {
   const navigate = useNavigate()
+  const location = useLocation()
+
+  const [getSearchResult, { loading, error, called }] =
+    useLazyQuery(GET_SEARCH_STORES)
 
   useEffect(() => {
-    const [_, query] = window.location.search.split('?query=')
+    const [_, query] = location.search.split('?query=')
+
     if (!query) {
       return navigate('/', {
         replace: true,
       })
     }
-  }, [])
+
+    getSearchResult({
+      variables: {
+        page: 1,
+        keyword: query,
+      },
+    }).then((res) => console.log(res.data.searchStore.stores))
+  }, [navigate, location])
+
+  console.log(loading, error, called)
 
   return (
     <section>
