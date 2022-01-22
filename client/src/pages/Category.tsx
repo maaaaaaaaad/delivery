7aba1fe9c55e977228f07f01973030bd2ac0826b
DONG WOONG OH <integral5137@gmail.com>
2022-01-22 18:50:08 +0900

    fix category component

--- client/src/pages/Category.tsx
+++ client/src/pages/Category.tsx
@@ -1,29 +1,20 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Empty from '../components/block/empty'
 import { useParams } from 'react-router-dom'
-import { useLazyQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import { GET_ONE_CATEGORY } from '../graphql/queries/queries'
 
 const Category = () => {
   const params = useParams()
 
-  const [getOneCategory, { loading, data, error, called }] =
-    useLazyQuery(GET_ONE_CATEGORY)
+  const { loading, error, data } = useQuery(GET_ONE_CATEGORY, {
+    variables: {
+      page: 1,
+      name: params.name,
+    },
+  })
 
-  useEffect(() => {
-    const categoryParams = params.name
-
-    if (!categoryParams) {
-      return
-    }
-
-    getOneCategory({
-      variables: {
-        page: 1,
-        name: categoryParams,
-      },
-    }).then((res) => console.log(res.data.getOneCategory.category))
-  }, [])
+  !loading && !error && data && console.log(data.getOneCategory.category)
 
   return (
     <section>
